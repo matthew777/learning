25a3f1b4826b18d263576108ddfb9ed0e9db591c
Matthew Hong <weiren.hong@gmail.com>
2016-08-20 23:46:27 +0800

    add io.c input.c

---
+++ C/input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char c;
+    printf("Please keyin a character, Ex: A \n");
+    scanf("%c", &c);
+
+    printf("The input character is %c \n", c);	
+
+    return 0;
+}
